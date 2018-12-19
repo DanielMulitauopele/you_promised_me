5f630ed6aadc339154bb838467b6e6a799bf5840
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-12-19 11:08:45 -0700

    Completes test for new promise paths, adds link to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'promises#index'
+  root 'promises#index'
 
-  resources :promises, only: [:show]
+  resources :promises, only: [:show, :new]
 end
---
+++ spec/features/promises/user_can_add_a_new_promise_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'user can add a new promise' do
+  it "should link to new promise page from index page" do
+    visit root_path
+
+    click_on "Track new promise!"
+
+    expect(current_path).to eq(new_promise_path)
+  end
+  it 'should allow user to send post action' do
+    visit new_promise_path
+
+    fill_in 'Input', with: "New Promise Information!"
+
+    click_on 'Submit'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("New Promise Information!")
+  end
+end
